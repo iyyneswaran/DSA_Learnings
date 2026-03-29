900ad7f86bb8ab24ae00abb5ec89457275ce2647
iyyneswaran <pdee5660@gmail.com>
2026-03-29 22:57:33 +0530

    Euclid algorithm for GCD

---
+++ euclid_gcd.py
@@ -0,0 +1,30 @@
+# Recursion without loop
+def euclid_gcd(a, b):
+    if a < b:
+        a, b = b, a
+    if a % b == 0:
+        return b
+    else:
+        diff = a - b
+        return euclid_gcd(max(b, diff), min(b, diff))
+print(euclid_gcd(int(input()), int(input())))
+
+# Recursion with while loop 
+def euclid_While_gcd(a, b):
+    if a < b:
+        a, b = b, a
+    while(a % b) != 0:
+        diff = a - b
+        a, b = max(b, diff), min(b, diff)
+    return b
+print(euclid_While_gcd(int(input()), int(input())))
+
+# Even better 
+def euclid_original(a, b):
+    if a < b:
+        a, b = b, a
+    if a % b == 0:
+        return b
+    else:
+        return euclid_original(b, a % b)
+print(euclid_original(int(input()), int(input())))
