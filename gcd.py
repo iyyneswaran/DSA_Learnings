1d11af475ef93b9d8223134c7f42cff78f512bf8
iyyneswaran <pdee5660@gmail.com>
2026-03-29 19:22:23 +0530

    Refractor: Optimized the gcd.py

--- gcd.py
+++ gcd.py
@@ -6,3 +6,13 @@ def gcd(x,y):
             cf.append(i)
     return cf[-1] # or return max(cf)
 print(gcd(int(input()), int(input())))
+
+
+# A little more optimized version by eliminating the lists:
+def optimized_gcd(a, b):
+    gcd_value = 1
+    for i in range(2, min(a,b)):
+        if a % i == 0 and b % i == 0:
+            gcd_value = i
+    return gcd_value
+print(optimized_gcd(int(input()), int(input())))
